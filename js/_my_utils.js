5e88ad4048fc2bb2991508450f4e68ddd44e1568
petr0vsk <petr0vskjy.aleksander@gmail.com>
2015-02-03 09:47:50 +0400

    Первоналачальная разметка страницы: заголовок, окно ввода и контейнер для заметок.

--- js/_my_utils.js
+++ js/_my_utils.js
@@ -1,3 +1,39 @@
 /**
  * Created by Татьяна on 25.01.15.
  */
+
+
+
+
+$( document ).ready(function() {
+    var db = new PouchDB('todos');
+    var remoteCouch = false;
+    function addTodo(text) {
+        var todo = {
+            _id: new Date().toISOString(),
+            title: text,
+            completed: false
+        };
+        db.put(todo, function callback(err, result) {
+            if (!err) {
+                console.log('Successfully posted a todo!');
+            }
+        });
+    }
+
+   // основной цикл - изменение формы
+   $("#_go_form").change(function(){
+
+       var  _text_to_do = $("#_go_form").val();
+       $("._list_to_do").append("<li class='_item'>" + _text_to_do + "</li>");
+       addTodo(_text_to_do);
+       $("#_go_form").val(" ");
+
+    });   // #_go_form
+
+
+
+    //var db = new PouchDB('todos');
+    //var remoteCouch = false;
+
+});
