db078d0ba4ed750a1e12c8f370607cb26cdd7f14
petr0vsk <petr0vskjy.aleksander@gmail.com>
2015-02-22 17:32:15 +0300

    Подключили шаблон handlebars и добавление-удаление записией в БД

--- js/_my_utils.js
+++ js/_my_utils.js
@@ -4,68 +4,80 @@
 
 $( document ).ready(function() {
 
-    var db = new PouchDB('todos');
+
+    // Создадим базу данных
+    var db = new PouchDB('_to_do_DB');
     var remoteCouch = false;
-    function addTodo(text) {
-        var todo = {
-            _id: new Date().toISOString(),
-            title: text,
-            completed: false
-        };
-        db.put(todo, function callback(err, result) {
-            if (!err) {
-                console.log('Successfully posted a ' +  todo.title);
-            }
-        });
-    }
-    // Show the current list of todos by reading them from the database
-    // вывод на экран всей базы данных
-    $("#_show_btn").click(function(){
-        console.log("_show_btn click");
-        showTodos();
-    });
-    //------------
-    function showTodos() {
+
+    // объявим шаблоны handlebars
+    var template   =  Handlebars.compile( $('#entry-template').html() );
 
 
-        db.allDocs({include_docs:true, descending:true}, function(err, doc){
-             //console.log(doc.title);
-            redrawTodosUI(doc.rows);
-        });
-
-
-    }
-    function redrawTodosUI(todos) {
-        console.log("redrawTodosUI run!");
-        todos.forEach(function(todo) {
-            $("._list_to_do").append("<li class='_item'>" + todo.doc.title + "</li>");
-            //console.log(todo.doc.title);
-        });
-    }
-    // основной цикл - добавление новых записей в БД
-   $("#_go_form").change(function(){
-
-       var  _text_to_do = $("#_go_form").val();
-       $("._list_to_do").append("<li class='_item'>" + _text_to_do + "</li>");
-       addTodo(_text_to_do);
-       $("#_go_form").val('');
-
-    });   // #_go_form
-
-    $("#_clear_html_btn").click(function(){
-        $("._item").remove();
-
-    })
-        // очистка поля html-формы И БД
-      $("#_clear_db_btn").click(function(){
-        PouchDB.destroy('todos', function(err, info){});  // тотальное уничтожение базы данных
-
-        }); //#_clear_db_btn
 
     db.info().then(function (info) {
-        console.log("--- инфа по базе ---");
         console.log(info);
+    })
+    //добавим запись в базу данных
+    function _add_Todo(text) {
+        var todo = {
+            _id: new Date().toISOString(),
+            title: text
+        };
+         db.put(todo, function callback(err, result){
+            if(!err){
+                console.log('Todo Posted! ' + todo.title );
+            }
+        });
+
+        $("#_workplace").append(template(todo)).children().last().data("id", todo._id);
+    }
+    //вывод всех записей на экран
+    $("#_show_all").click(function(){
+           db.allDocs({include_docs:true, descending:true}, function(err, doc){
+            redrawTodosUI(doc.rows);
+        });
     });
+    // выведем в форму все записи БД, добавим в поле элемента <li> _id записи для поиска документа без обращения к БД
+    function redrawTodosUI(todos) {
+        $("._item").remove();
+        todos.forEach(function(todo) {
+            //console.log("todo.doc.title -> " + todo.doc.title + "; todo.doc._id -> " + todo.doc._id);
+            var todo = {
+                title: todo.doc.title,
+                _id:  todo.doc._id
+                       };
+
+            $("#_workplace").append(template(todo)).children().last().data("id", todo._id);
+            //$(this).children().last().data("id", todo.doc._id);
+        });
+    }
+    // очистка поля html-формы, БД не трогаем
+    $("#_erase_all").click(function(){
+        $("._item").remove();
+    });
+
+    // удалим запись из БД после клика по заметке на экране
+    $('ul').on('click', 'li',  function(){
+        var doc_id = $(this).data("id");
+        db.query(function(doc, emit) {
+            if (doc._id === doc_id) {
+                emit(doc);
+                db.remove(doc);
+            }
+        }, function(err, results) {
+                    });
+                    $(this).remove();
+    });
+    // основной цикл - добавление новых записей в БД
+   $("input").change(function(){
+         var  _text_to_do = $("input").val();
+        // $(".gridster ul").append("<li data-row='1' data-col='1' data-sizex='1' data-sizey='1'>" + _text_to_do + "</li>");
+         _add_Todo(_text_to_do);
+         $("input").val('');
+    });
+
+
+
 
 
 });  // end main()
